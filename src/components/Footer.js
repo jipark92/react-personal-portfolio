68c3a36e1a2f253bce831384d1acd0706106e617
ji <jiyongprk@gmail.com>
2022-04-07 11:28:39 -0400

    change foot text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="footer-container">
-            <h3>Created by Norfkorean</h3>
+            <h3>Created by Ji Park/NorfKorean</h3>
         </footer>
     )
 }
