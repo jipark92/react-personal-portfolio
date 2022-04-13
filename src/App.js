9062afc332a20cd9cdda39b3f85b0de591f4f26b
ji <jiyongprk@gmail.com>
2022-04-13 19:58:49 -0400

    text color to if state

--- src/App.js
+++ src/App.js
@@ -32,20 +32,20 @@ export default function App() {
 
   const increaseTextSize = () => {
     if (pageNumber === 0 ) {
-      return <h3 className='title-select'><h2 Style="color:white">ABOUT ME</h2> / Skills / Contact </h3>
+      return <h3 className='title-select'><h2 Style="color:white">ABOUT ME / </h2> Skills / Contact </h3>
     } else if (pageNumber === 1 ){
-      return <h3 className='title-select'>About Me / <h2 Style="color:white">SKILLS</h2> / Contact </h3>
+      return <h3 className='title-select'>About Me<h2 Style="color:white">/ SKILLS /</h2>Contact </h3>
     } else if (pageNumber === 2 ){
-      return <h3 className='title-select'>About Me / Skills / <h2 Style="color:white">CONTACT</h2> </h3>
+      return <h3 className='title-select'>About Me / Skills<h2 Style="color:white">/ CONTACT</h2> </h3>
     }
   }
 
   return (
     <div className="main-container">
       <div className='slide-btn'>
-        <button onClick={prevBtn}>prev</button>
+        <button onClick={prevBtn}>&#8678;</button>
         <h3>{increaseTextSize()}</h3>
-        <button onClick={nextBtn}>next</button>
+        <button onClick={nextBtn}>&#8680;</button>
       </div>
       <div className='slide-container'>
         <div className="slide">
