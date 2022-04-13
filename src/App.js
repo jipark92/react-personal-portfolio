cb75b8f63335f9ec842d6a1d293f35c51f5a340f
ji <jiyongprk@gmail.com>
2022-04-13 18:35:49 -0400

    add conntact.js, add prev/next btn to slide and style the containers

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React from 'react'
 import RouteSwitch from './RouteSwitch'
 import AboutMe from './components/aboutme/AboutMe'
 import Footer from './components/Footer'
+import Contacts from './components/contact/Contacts'
 
 export default function App() {
+
+  const [pageNumber, setPageNumber] = React.useState(0)
+
+  const prevBtn = () => {
+    const slides = document.querySelectorAll('.slide')
+    if (pageNumber === 1 ){
+      slides[0].scrollIntoView();
+      setPageNumber(0);
+    } else if ( pageNumber === 2) {
+      slides[1].scrollIntoView()
+      setPageNumber(1)
+    }
+  }
+
+  const nextBtn = () =>{
+    const slides = document.querySelectorAll('.slide')
+    if (pageNumber === 0 ){
+      slides[1].scrollIntoView();
+      setPageNumber(1);
+    } else if (pageNumber === 1) {
+      slides[2].scrollIntoView()
+      setPageNumber(2)
+    }
+  }
+
+  
+
   return (
     <div className="main-container">
+      <div className='slide-btn'>
+        <button onClick={prevBtn}>prev</button>
+        <button onClick={nextBtn}>next</button>
+      </div>
       <div className='slide-container'>
-        <div className="about-me-container">
+        <div className="slide">
           <AboutMe/>
         </div>
-        <div className='router-container'>
+        <div className='slide'>
           <RouteSwitch/>
+        </div>
+        <div className='slide'>
+          <Contacts/>
         </div>
       </div>
       <Footer/>
---
+++ src/components/contact/Contacts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contacts() {
+    return (
+        <div className='contact-container'>
+            <h2>contacts</h2>
+            
+        </div>
+    )
+}
