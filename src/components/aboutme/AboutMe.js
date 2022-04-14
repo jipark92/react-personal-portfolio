9436a36fabc2ab6e2a971a0d94f4410881c071a1
ji <jiyongprk@gmail.com>
2022-04-13 21:11:23 -0400

    add more lorem to description

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -4,21 +4,14 @@ export default function AboutMe() {
   return (
     <div className='about-me'>
       <div className='img-container'>
-        <img src="https://img.icons8.com/dotty/344/name.png" alt='profile-picture'/>
+        <img src="https://img.icons8.com/dotty/344/name.png" alt='profile-pictures'/>
       </div>
       <div className='name-container'>
         <h2>John Doe</h2>
         <h3>Software Developer</h3>
       </div>
       <div className='description'>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud .</h2>
-      </div>
-      <div className='social-container'>
-        <a href='https://t.co/2Ed6qCNfkO' target="_blank"><img src="https://img.icons8.com/external-justicon-flat-justicon/344/external-linkedin-social-media-justicon-flat-justicon.png" alt="LinkedIn" className='social-logos'/></a>
-        <a href="https://github.com/norfkorean" target="_blank"><img src="https://img.icons8.com/color/344/github--v1.png" alt="GitHub" className='social-logos'/></a>
-        <a href="https://twitter.com/norfkorean" target="_blank"><img src="https://img.icons8.com/stickers/344/twitter.png" alt="Twitter" className='social-logos'/></a>
-        <a href="https://www.tiktok.com/@norfkorean" target="_blank"><img src="https://img.icons8.com/ios-glyphs/344/tiktok.png" alt="TikTok" className='social-logos'/></a>
-        <a href="https://instagram.com/bboyji_" target="_blank"><img src="https://img.icons8.com/fluency/344/instagram-new.png" alt="Instagram" className='social-logos'/></a>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.Lorem ipsum dolor sit amet, consectetur.Lorem ipsum dolor sit amet, consectetur.</h2>
       </div>
     </div>
   )
