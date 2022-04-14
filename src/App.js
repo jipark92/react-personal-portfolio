5f9de65c70f6f54ca784269b98c84fdf2ece0f30
ji <jiyongprk@gmail.com>
2022-04-13 21:08:08 -0400

    change h3 containers to div

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ export default function App() {
 
   const increaseTextSize = () => {
     if (pageNumber === 0 ) {
-      return <h3 className='title-select'><h2 Style="color:white">ABOUT ME / </h2> Skills / Contact </h3>
+      return <div className='title-select'><h2 Style="color:white">ABOUT ME / </h2> Skills / Contact </div>
     } else if (pageNumber === 1 ){
-      return <h3 className='title-select'>About Me<h2 Style="color:white">/ SKILLS /</h2>Contact </h3>
+      return <div  className='title-select'>About Me<h2 Style="color:white">/ SKILLS /</h2>Contact </div>
     } else if (pageNumber === 2 ){
-      return <h3 className='title-select'>About Me / Skills<h2 Style="color:white">/ CONTACT</h2> </h3>
+      return <div className='title-select'>About Me / Skills<h2 Style="color:white">/ CONTACT</h2> </div>
     }
   }
 
@@ -45,7 +45,7 @@ export default function App() {
       <div className='slide-btn'>
         <button onClick={prevBtn}>&#8678;</button>
         <h3>{increaseTextSize()}</h3>
-        <button onClick={nextBtn}>&#8680;</button>
+        <button className='next-slide' onClick={nextBtn}>&#8680;</button>
       </div>
       <div className='slide-container'>
         <div className="slide">
