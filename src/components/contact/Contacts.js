6a26dd7a39a0f0f1c4378861c5e3e5c4da7cea67
ji <jiyongprk@gmail.com>
2022-04-13 21:12:04 -0400

    move social from about me to here

--- src/components/contact/Contacts.js
+++ src/components/contact/Contacts.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Contacts() {
     return (
         <div className='contact-container'>
-            <h2>contacts</h2>
+            <div className='social-container'>
+                <a href='https://t.co/2Ed6qCNfkO' target="_blank"rel="noreferrer"><img src="https://img.icons8.com/external-justicon-flat-justicon/344/external-linkedin-social-media-justicon-flat-justicon.png" alt="LinkedIn" className='social-logos'/></a>
+                <a href="https://github.com/norfkorean" target="_blank"rel="noreferrer"><img src="https://img.icons8.com/color/344/github--v1.png" alt="GitHub" className='social-logos'/></a>
+                <a href="https://twitter.com/norfkorean" target="_blank"rel="noreferrer"><img src="https://img.icons8.com/stickers/344/twitter.png" alt="Twitter" className='social-logos'/></a>
+                <a href="https://www.tiktok.com/@norfkorean" target="_blank"rel="noreferrer"><img src="https://img.icons8.com/ios-glyphs/344/tiktok.png" alt="TikTok" className='social-logos'/></a>
+                <a href="https://instagram.com/bboyji_" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluency/344/instagram-new.png" alt="Instagram" className='social-logos'/></a>
+            </div>
         </div>
     )
 }
